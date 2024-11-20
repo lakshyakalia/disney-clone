3167e95b078eee30d98615eb69b7250f4adcc0a4
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2024-11-20 14:15:37 -0500

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes exact path="/">
           <Route exact path="/" element={<Login />}></Route>
         </Routes>
